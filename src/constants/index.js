3e39ceeb75453c760256e1fa87d6f2656fabfa36
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-05 11:00:11 +0300

    Constants file

---
+++ src/constants/index.js
@@ -0,0 +1,56 @@
+export const INPUTS_REGISTER = [
+  {
+    id: 'username',
+    name: 'username',
+    type: 'text',
+    label: 'User Name',
+    placeholder: 'Enter your usernam',
+  },
+  {
+    id: 'email',
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    placeholder: 'Enter email address',
+  },
+  {
+    id: 'password',
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    placeholder: 'Enter Password',
+  },
+  {
+    id: 'repassword',
+    name: 'repassword',
+    type: 'password',
+    label: 'rePassword',
+    placeholder: 'Enter rePassword',
+  },
+  {
+    id: 'checkBoxInput',
+    name: 'checkBoxInput',
+    type: 'checkbox',
+    label: 'I agree to terms & conditions',
+    
+  },
+];
+
+
+
+export const INPUTS_LOGIN = [
+  {
+    id: 'email',
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    placeholder: 'Enter email address',
+  },
+  {
+    id: 'password',
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    placeholder: 'Enter Password',
+  },
+];
