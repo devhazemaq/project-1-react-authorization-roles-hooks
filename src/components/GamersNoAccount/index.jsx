b1c16e277d2a06534002f92489f55d516938b924
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-05 10:56:05 +0300

    component GamersNoAccount

---
+++ src/components/GamersNoAccount/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.css'
+
+const GamersNoAccount = () => {
+  return (
+    <div className='gamersnoaccount__contain'>
+      Don&rsquo;t have an account? <span>Register</span> 
+    </div>
+  )
+};
+
+export default GamersNoAccount;
