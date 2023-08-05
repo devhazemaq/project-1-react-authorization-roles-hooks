cbc58b60faef8db48f63e00febef3009fdbe9ab8
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-05 19:22:24 +0300

    component  GamersTextLogin

---
+++ src/components/Gamers/GamersTextLogin/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './style.css'
+
+const GamersTextLogin = () => {
+  return (
+    <div className='gamerstextlogin__main'>
+      <h2>Join the game!</h2>
+      <p>Go inside the best gamers social network!</p>
+    </div>
+  )
+};
+
+export default GamersTextLogin;
