dff03cb9bda3af5acaecfc978315a5a47a1c4d4d
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-05 20:21:44 +0300

    GamersTextRegister

---
+++ src/components/Gamers/GamersTextRegister/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+
+const GamersTextRegister = () => {
+  return (
+    <div className='gamerstextregister__main'>
+      <h2>Register Individual Account!</h2>
+      <p>For the purpose of gamers regulation, your <br /> details are required.</p>
+    </div>
+  )
+};
+
+export default GamersTextRegister;
