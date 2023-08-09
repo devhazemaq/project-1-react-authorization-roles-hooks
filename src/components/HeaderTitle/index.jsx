d7b17382c8322299b0cd918e320041f2c2dfb5bb
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-10 00:02:18 +0300

    HeaderTitle

---
+++ src/components/HeaderTitle/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './style.css';
+
+
+const HeaderTitle = ({titleText}) => {
+  return (
+    <h2 className='headertit__main'>{titleText}</h2>
+  )
+};
+
+export default HeaderTitle;
