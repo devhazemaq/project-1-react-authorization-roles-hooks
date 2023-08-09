3368d7aafd3607b644e8e90aeabf99dff62d28b0
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-09 23:59:01 +0300

    useThemeContext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 // import LoginPage from "./pages/LoginPage";
 // import SginupPage from "./pages/SginupPage";
+import { useThemeContext } from "./context/ThemeContext";
 
 import MainLayout from "./components/MainLayout";
+// import HazPage from "./pages/HazPage";
 import  Router  from "./router";
 
 
+
 function App() {
+
+  const {theme } = useThemeContext();
+
+
   return (
-    <div className="App">
+    <div className={theme}>
       {/* < SginupPage />
       < LoginPage /> */}
       <MainLayout>
         <Router />
       </MainLayout>
+
+      {/* <HazPage /> */}
 
     </div>
   );
