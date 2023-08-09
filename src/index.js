753a0f5b5c6dccafff104147905d4ae70cf02604
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-09 23:58:41 +0300

    Add context for project to acsses for all

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthContext';
+import ThemeProvider  from './context/ThemeContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
