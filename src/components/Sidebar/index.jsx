c9be83befa93a35784256eca65c28a01d634cf8b
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-18 21:59:21 +0300

    edit and improvements for sidepar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,60 +1,81 @@
-import React, { useEffect } from 'react'
-// import { NavLink, useNavigate } from 'react-router-dom';
-// import { PATHS } from '../../router/paths';
+import React from 'react'
 import './style.css';
 import { useThemeContext } from '../../context/ThemeContext';
 import { useAuthContext } from '../../context/AuthContext';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { ROLES } from '../../constants';
+import { ROLES, THEMES } from '../../constants';
 
 
 
 
 const Sidebar = () => {
-  const { role, user, logout } = useAuthContext();
+  const { role, logout } = useAuthContext(); //, user
   const { theme, toggleTheme } = useThemeContext();
   const pathImage = "/assets/images/";
+  const pathImageIcon = "/assets/images/forSideber/";
 
 
   const navigate = useNavigate()
 
   const handleLogout = () => {
     logout();
-    console.log(role + ' handleLogout');
+    // console.log(role + ' handleLogout');
     navigate("/")
   };
 
-  useEffect(() => { }, [])
+  // useEffect(() => { }, [])
 
 
-  console.log(role);
-  console.log(user);
-  console.log(theme);
+  // console.log(role);
+  // console.log(user);
+  // console.log(theme);
   return (
     <div className='sidebar__main'>
-      <img src={`${pathImage}forSideber/svgviewer-output1.svg`} alt="svgviewer-output1" className='svgviewer-output1' />
+      {role === ROLES.ADMIN ?
+        <NavLink to={"/admin"}>
+          <img src={`${pathImage}forSideber/svgviewer-output1.svg`} alt="svgviewer-output1" className='svgviewer-output1' />
+        </NavLink>
+        :
+        <NavLink to={"/user"}>
+          <img src={`${pathImage}forSideber/svgviewer-output1.svg`} alt="svgviewer-output1" className='svgviewer-output1' />
+        </NavLink>
+      }
+
+
       <div className="sidebarIcon__contain">
         <ul className='ulIconSidepar'>
           <li>
             {role === ROLES.ADMIN ?
-
               <NavLink to={"/admin/users"}>
-                <img src={`${pathImage}forSideber/svgviewer-output3Adjust.svg`} alt="svgviewer-output3Adjust" className='adjust' />
+                {theme === THEMES.LIGHT ?
+                  <img
+                    src={`${pathImageIcon}settings-gray.svg`}
+                    alt="settings" className='adjust'
+                  />
+                  :
+                  <img
+                    src={`${pathImageIcon}settings-white.svg`}
+                    alt="settings"
+                    className='adjust'
+                  />
+                }
               </NavLink>
-
               :
               <></>
             }
           </li>
           <li>
-            {/* <NavLink to={PATHS.LOGIN} > */}
-            <img src={`${pathImage}forSideber/exit.png`}
-              alt="exit" className='exit'
-              onClick={handleLogout}
-
-            />
-
-
+            {theme === THEMES.LIGHT ?
+              <img src={`${pathImageIcon}exit-gray.svg`}
+                alt="exit" className='exit'
+                onClick={handleLogout}
+              />
+              :
+              <img src={`${pathImageIcon}exit-white.svg`}
+                alt="exit" className='exit'
+                onClick={handleLogout}
+              />
+            }
           </li>
         </ul>
       </div>
@@ -70,7 +91,7 @@ const Sidebar = () => {
           src={`${pathImage}forSideber/svgviewer-output5-sun-light.svg`}
           alt="sun"
         />
-        
+
       </div>
     </div>
 
