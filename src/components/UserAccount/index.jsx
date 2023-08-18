4b1941364a1d73f08126e81ad024870f85186294
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-18 22:01:41 +0300

    fix bug show name of user

--- src/components/UserAccount/index.jsx
+++ src/components/UserAccount/index.jsx
@@ -1,26 +1,34 @@
 
+import { NavLink } from 'react-router-dom';
 import './style.css';
-import useAuth from '../hooks/useAuth';
+// import { ROLES } from '../../constants';
+// import useAuth from '../../hooks/useAuth';
 
 
 const UserAccount = () => {
 
-  const {user} = useAuth();
-  
-
+  // const { user } = useAuth();
   const pathImage = "/assets/images/";
   
-  
+  let dataUser = JSON.parse(localStorage.getItem("user")) 
+
+  // console.log(dataUser);  
 
   return (
     <div className='useracc__main'>
       <div className="useracc__welcome">
         <p >Welcme back,</p>
-        <p >{user?.name || 'user'}!</p>
+        <p >{dataUser?.name}!</p>
       </div>
-      {/* <NavLink to={"/user/profile"}> */}
-        <img src={`${pathImage}profile_img.png`} alt="profile_img" className='profile__img'   />
-      {/* </NavLink> */}
+      {dataUser?.isAdmin === true ? 
+        <NavLink to={"/admin/user/profile"}>
+          <img src={`${pathImage}profile_img_admin.png`} alt="profile_img" className='profile__img'   />
+        </NavLink>
+      : 
+        <NavLink to={"/user/profile"}>
+          <img src={`${pathImage}profile_img.png`} alt="profile_img" className='profile__img'   />
+        </NavLink>
+      }
     </div>
   )
 };
