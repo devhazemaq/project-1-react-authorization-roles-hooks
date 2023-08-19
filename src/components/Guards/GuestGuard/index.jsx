3a4e15e14cbba8f6f516ac26e7c2caed65c4d14d
devhazemaq <hazemahmedqindeel@gmail.com>
2023-08-19 11:24:04 +0300

    fix bug After you log in to the site and then close it and then open it, it displays a white page, and it must display the main page, whether it is for the user or for the admin

--- src/components/Guards/GuestGuard/index.jsx
+++ src/components/Guards/GuestGuard/index.jsx
@@ -1,18 +1,34 @@
 
 import { useNavigate } from 'react-router-dom';
 import { ROLES } from '../../../constants';
-import { PATHS } from '../../../router/paths';
+// import { PATHS } from '../../../router/paths';
 import { useAuthContext } from '../../../context/AuthContext';
+import { useEffect } from 'react';
+
+
 
 const GuestGuard = ({children}) => {
+
   
   const {role} = useAuthContext();
   const navigate = useNavigate();
   console.log(role)
-  if (role === ROLES.ADMIN) return navigate(PATHS.ADMIN.ROOT);
-  if (role === ROLES.USER) return navigate(PATHS.USER.ROOT);
+  // if (role === ROLES.ADMIN) return navigate('/admin'); //PATHS.ADMIN.ROOT
+  // if (role === ROLES.USER) return navigate(PATHS.USER.ROOT);
   
+  useEffect(() => {
+    if (role === ROLES.ADMIN) {
+      navigate('/admin'); 
+    }
+  }, [role, navigate]);
   
+  useEffect(() => {
+    if (role === ROLES.USER) {
+      navigate('/user'); 
+    }
+  }, [role, navigate]);
+  
+
   return children;  
 }
 
